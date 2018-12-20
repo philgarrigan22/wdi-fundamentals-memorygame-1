4e544c54ffb475a4928aad622b08b208bb7eaa61
David Ko <david.holy.ko@gmail.com>
2018-12-20 13:05:56 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -6,3 +6,20 @@ var cardThree = "queen";
 var cardFour = "queen";
 
 console.log("User flipped " + cardOne);
+
+cards = [cardOne, cardTwo, cardThree, cardFour];
+cardsInPlay = [];
+
+cardsInPlay.push(cardOne);
+console.log(cardsInPlay);
+cardsInPlay.push(cardTwo);
+console.log(cardsInPlay);
+
+if (cardsInPlay.length == 2){
+  if (cardsInPlay[0] === cardsInPlay[1]){
+    alert("You found a match!");
+  }
+  else {
+    alert("Try again!");
+  }
+}
